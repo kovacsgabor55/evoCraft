e6c274c26400c79e70409aeda0514243ef9e0e5b
Balint Sedlak <sedlak.balint@gmail.com>
2018-02-07 21:32:37 +0100

    merge

--- EvoCraft2.UI/MainMenu.xaml.cs
+++ EvoCraft2.UI/MainMenu.xaml.cs
@@ -1,21 +1,13 @@
 ﻿using EvoCraft2.Common;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Media;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Xml.Serialization;
 
 namespace EvoCraft2.UI
@@ -26,16 +18,43 @@ namespace EvoCraft2.UI
     public partial class MainMenu : Page
     {
         SoundPlayer soundPlayer = new SoundPlayer();
+        private static bool isYellow = true;
+        private static Timer timer;
 
         public MainMenu()
         {
             InitializeComponent();
 
             //Sounds.StartMenuMusic();
+
+            timer = new Timer(timerTick, null, 0, 50);
+        }
+
+        private void timerTick(object state)
+        {
+            if (isYellow)
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    newFeatureLabel.Foreground = Brushes.Aqua;
+                    multiplayerButton.Foreground = Brushes.Aqua;
+                });
+                isYellow = !isYellow;
+            }
+            else
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    newFeatureLabel.Foreground = Brushes.Yellow;
+                    multiplayerButton.Foreground = Brushes.Yellow;
+                });
+                isYellow = !isYellow;
+            }
         }
 
         private void exitButton_Click(object sender, RoutedEventArgs e)
         {
+            timer.Dispose();
             //Sounds.StopMenuMusic();
             //Sounds.ShutDown();
             System.Threading.Thread.Sleep(3000);
@@ -44,12 +63,14 @@ namespace EvoCraft2.UI
 
         private void creditsButton_Click(object sender, RoutedEventArgs e)
         {
+            timer.Dispose();
             Credits credits = new Credits();
             this.NavigationService.Navigate(credits);
         }
 
         private void optionsButton_Click(object sender, RoutedEventArgs e)
         {
+            timer.Dispose();
             Options options = new Options();
             this.NavigationService.Navigate(options);
 
@@ -71,12 +92,14 @@ namespace EvoCraft2.UI
 
         private void tutorialButton_Click(object sender, RoutedEventArgs e)
         {
+            timer.Dispose();
             TutorialsPage page = new TutorialsPage();
             this.NavigationService.Navigate(page);
         }
 
         private void singleplayerButton_Click(object sender, RoutedEventArgs e)
         {
+            timer.Dispose();
             //Sounds.StopMenuMusic();
             //LoadingScreen page = new LoadingScreen();
             //RunningGame page = new RunningGame();
@@ -86,6 +109,7 @@ namespace EvoCraft2.UI
 
         private void multiplayerButton_Click(object sender, RoutedEventArgs e)
         {
+            timer.Dispose();
             Multiplayer page = new Multiplayer();
             NavigationService.Navigate(page);
         }
